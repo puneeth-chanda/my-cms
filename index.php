9d96d73e5e9ec52db78b40425aa1d41a99471edd
puneeth2001 <puneethchanda2001@gmail.com>
2019-02-21 17:10:01 +0530

    phpself

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php
+<html>
+    <body>
+    <?php
     /*echo "Welcome" . $_GET["name"];
     echo "<br>";
     echo"Your email address is:" . $_GET["email"];
@@ -45,4 +47,40 @@
     }
 
     ?>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
+Name: <input type="text" name="name">
+<span class="error">* <?php echo $nameErr;?></span>
+<br><br>
+E-mail:
+<input type="text" name="email">
+<span class="error">* <?php echo $emailErr;?></span>
+<br><br>
+Website:
+<input type="text" name="website">
+<span class="error"><?php echo $websiteErr;?></span>
+<br><br>
+Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+<br><br>
+Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+<input type="radio" name="gender" value="other">Other
+<span class="error">* <?php echo $genderErr;?></span>
+<br><br>
+<input type="submit" name="submit" value="Submit"> 
+        </form>
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
+    ?>
+    </body>
+</html>
